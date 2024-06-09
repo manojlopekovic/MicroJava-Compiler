e2a0b1076867d8bdc65c3c8fec444e5282fe0d77
Manojlo Pekovic <manojlop@veriests.com>
2024-06-09 21:52:31 +0200

    Added SymTable

--- test/rs/ac/bg/etf/pp1/MJParserTest.java
+++ test/rs/ac/bg/etf/pp1/MJParserTest.java
@@ -28,7 +28,7 @@ public class MJParserTest {
 		
 		Reader br = null;
 		try {
-			File sourceCode = new File("test/test301.mj");
+			File sourceCode = new File("test/program.mj");
 			log.info("Compiling source file: " + sourceCode.getAbsolutePath());
 			
 			br = new BufferedReader(new FileReader(sourceCode));
@@ -58,9 +58,6 @@ public class MJParserTest {
 				return;
 			} else {
 				log.info(" SUCCESFULL PARSING ");
-				log.info(" Print count calls = " + v.printCallCount);
-
-				log.info(" Deklarisanih promenljivih ima = " + v.varDeclCount);
 			}
 		} 
 		finally {
