ac201b70665c87491b5fc4b24d5f646ac80a675f
Manojlo Pekovic <manojlop@veriests.com>
2024-06-12 21:36:45 +0200

    Finished phase 3, semantic analysis for grade A.

--- src/rs/ac/bg/etf/pp1/SemanticAnalyzer.java
+++ src/rs/ac/bg/etf/pp1/SemanticAnalyzer.java
@@ -7,15 +7,20 @@ import rs.etf.pp1.symboltable.Tab;
 import rs.etf.pp1.symboltable.concepts.Obj;
 import rs.etf.pp1.symboltable.concepts.Struct;
 
-// Visitor adaptor is apstract class that implements Visit interface
+// Visitor adaptor is abstract class that implements Visit interface
 // Visit interface has methods visit, for each node in tree, that should be overwritten
 public class SemanticAnalyzer extends VisitorAdaptor {
+	
+//	Todo : Do report info from specification of project
 
 	private Obj myProg;
 	private Struct currentType;
 	private int constant;
 	private Struct constantType;
 	private Struct boolType = Tab.find("bool").getType();
+	private Struct intType = Tab.intType;
+	private Struct charType = Tab.charType;
+	private Struct noType = Tab.noType;
 	private boolean ismain = false;
 	private Obj currentMethod;
 
@@ -23,36 +28,115 @@ public class SemanticAnalyzer extends VisitorAdaptor {
 //	Semantic pass code, context conditions
 //****************************************************************************************************************
 
+//	Statement
+	
+	@Override
+	public void visit(StmtRead stmtRead) {
+//		Check if variable, element of an array [Field inside class for C]
+		if(stmtRead.getDesignator().obj.getKind() != Obj.Var && stmtRead.getDesignator().obj.getKind() != Obj.Elem) {
+			error_report("Trying to read: " + stmtRead.getDesignator().obj.getName() + " which is of unreadable type", stmtRead);
+			return;
+		}
+		Struct typeStruct = stmtRead.getDesignator().obj.getType();
+		if(!typeStruct.equals(intType) && !typeStruct.equals(charType)  && !typeStruct.equals(boolType)) {
+			error_report("Trying to read: " + stmtRead.getDesignator().obj.getName() + " which is not integer, char or bool type", stmtRead);
+			return;
+		}
+	}
+	
+	@Override
+	public void visit(StmtPrintExpr stmtPrint) {
+//		Check if variable, element of an array [Field inside class for C]
+		Struct typeStruct = stmtPrint.getExpr().struct;
+		if(typeStruct.getKind() != Struct.Array && !typeStruct.equals(intType) && !typeStruct.equals(boolType)  && !typeStruct.equals(charType)) {
+			error_report("Trying to print: " + typeStruct.toString() + " which is not array, integer, char or bool type", stmtPrint);
+			return;
+		}
+	}
+	
+	@Override
+	public void visit(StmtPrintNum stmtPrint) {
+//		Check if variable, element of an array [Field inside class for C]
+		Struct typeStruct = stmtPrint.getExpr().struct;
+		if(typeStruct.getKind() != Struct.Array && !typeStruct.equals(intType) && !typeStruct.equals(boolType)  && !typeStruct.equals(charType)) {
+			error_report("Trying to print: " + typeStruct.toString() + " which is not array, integer, char or bool type, with params", stmtPrint);
+			return;
+		}
+	}
+	
+	
+//	Designator statement
+	@Override
+	public void visit(DStmtDesAExpr dStmtDesAExpr) {
+//		Check if variable, element of an array [Field inside class for C]
+		if(dStmtDesAExpr.getDesignator().obj.getKind() != Obj.Var && dStmtDesAExpr.getDesignator().obj.getKind() != Obj.Elem) {
+			error_report("Trying to assign to: " + dStmtDesAExpr.getDesignator().obj.getName() + " which is of unassignable type", dStmtDesAExpr);
+			return;
+		}
+		if(!dStmtDesAExpr.getExpr().struct.assignableTo(dStmtDesAExpr.getDesignator().obj.getType())) {
+			error_report("Trying to assign variable type: " + dStmtDesAExpr.getExpr().struct.getKind() + " to: " + dStmtDesAExpr.getDesignator().obj.getName(), dStmtDesAExpr);
+			return;
+		}
+	}
+
+	@Override
+	public void visit(DStmtDesInc dStmtDesInc) {
+//		Check if variable, element of an array [Field inside class for C]
+		if(dStmtDesInc.getDesignator().obj.getKind() != Obj.Var && dStmtDesInc.getDesignator().obj.getKind() != Obj.Elem) {
+			error_report("Trying to increment: " + dStmtDesInc.getDesignator().obj.getName() + " which is of unassignable type", dStmtDesInc);
+			return;
+		}
+		if(!dStmtDesInc.getDesignator().obj.getType().equals(intType)) {
+			error_report("Operand for incrementing: " + dStmtDesInc.getDesignator().obj.getName() +" is not integer type", dStmtDesInc);
+			return;
+		}
+	}
+	
+	@Override
+	public void visit(DStmtDesDec dStmtDesDec) {
+//		Check if variable, element of an array [Field inside class for C]
+		if(dStmtDesDec.getDesignator().obj.getKind() != Obj.Var && dStmtDesDec.getDesignator().obj.getKind() != Obj.Elem) {
+			error_report("Trying to decrement: " + dStmtDesDec.getDesignator().obj.getName() + " which is of unassignable type", dStmtDesDec);
+			return;
+		}
+		if(!dStmtDesDec.getDesignator().obj.getType().equals(intType)) {
+			error_report("Operand for decrementing: " + dStmtDesDec.getDesignator().obj.getName() +" is not integer type", dStmtDesDec);
+			return;
+		}
+	}
+	
+	
+	
 //	Expr, Term
 	
 	@Override
 	public void visit(MulFactorFactor mulFactorFactor) {
 		if(mulFactorFactor.getMulFactor() instanceof MulFactorFactor) {
-			if(!mulFactorFactor.getMulFactor().struct.equals(Tab.intType)) {
+			if(!mulFactorFactor.getMulFactor().struct.equals(intType)) {
 				error_report("Trying to assign non-int value: " + mulFactorFactor.getMulFactor().struct + " in multiplication operation", mulFactorFactor);
-				mulFactorFactor.struct = Tab.noType;
+				mulFactorFactor.struct = noType;
 				return;
 			}
 		}
-		if(!mulFactorFactor.getFactor().struct.equals(Tab.intType)) {
+		if(!mulFactorFactor.getFactor().struct.equals(intType)) {
 			error_report("Trying to assign non-int value: " + mulFactorFactor.getFactor().struct + " in multiplication operation", mulFactorFactor);
-			mulFactorFactor.struct = Tab.noType;
+			mulFactorFactor.struct = noType;
 			return;
 		}
-		mulFactorFactor.struct = Tab.intType;
+		mulFactorFactor.struct = intType;
 	}
 	
 	@Override
 	public void visit(Term term) {
 		if(term.getMulFactor() instanceof MulFactorFactor) {
-			if(!term.getMulFactor().struct.equals(Tab.intType)) {
+			if(!term.getMulFactor().struct.equals(intType)) {
 				error_report("Trying to assign non-int value: " + term.getMulFactor().struct + "  in term", term);
-				term.struct = Tab.noType;
+				term.struct = noType;
 				return;
 			} 
-			if(!term.getFactor().struct.equals(Tab.intType)) {
+			if(!term.getFactor().struct.equals(intType)) {
 				error_report("Trying to assign non-int value: " + term.getFactor().struct + "  in term", term);
-				term.struct = Tab.noType;
+				term.struct = noType;
 				return;
 			}
 		}
@@ -62,32 +146,32 @@ public class SemanticAnalyzer extends VisitorAdaptor {
 	@Override
 	public void visit(AddTermTerm addTermTerm) {
 		if(addTermTerm.getAddTerm() instanceof AddTermTerm) {
-			if(!addTermTerm.getAddTerm().struct.equals(Tab.intType)) {
-				error_report("Trying to assign non-int value: " + addTermTerm.getAddTerm().struct + " in multiplication operation", addTermTerm);
-				addTermTerm.struct = Tab.noType;
+			if(!addTermTerm.getAddTerm().struct.equals(intType)) {
+				error_report("Trying to assign non-int value: " + addTermTerm.getAddTerm().struct + " in addition operation", addTermTerm);
+				addTermTerm.struct = noType;
 				return;
 			}
 		}
-		if(!addTermTerm.getTerm().struct.equals(Tab.intType)) {
-			error_report("Trying to assign non-int value: " + addTermTerm.getTerm().struct + " in multiplication operation", addTermTerm);
-			addTermTerm.struct = Tab.noType;
+		if(!addTermTerm.getTerm().struct.equals(intType)) {
+			error_report("Trying to assign non-int value: " + addTermTerm.getTerm().struct + " in addition operation", addTermTerm);
+			addTermTerm.struct = noType;
 			return;
 		}
-		addTermTerm.struct = Tab.intType;
+		addTermTerm.struct = intType;
 		
 	}
 	
 	@Override
 	public void visit(Expr expr) {
 		if(expr.getAddTerm() instanceof AddTermTerm) {
-			if(!expr.getAddTerm().struct.equals(Tab.intType)) {
-				error_report("Trying to assign non-int value: " + expr.getAddTerm().struct + "  in term", expr);
-				expr.struct = Tab.noType;
+			if(!expr.getAddTerm().struct.equals(intType)) {
+				error_report("Trying to assign non-int value: " + expr.getAddTerm().struct + "  in expr", expr);
+				expr.struct = noType;
 				return;
 			} 
-			if(!expr.getTerm().struct.equals(Tab.intType)) {
-				error_report("Trying to assign non-int value: " + expr.getTerm().struct + "  in term", expr);
-				expr.struct = Tab.noType;
+			if(!expr.getTerm().struct.equals(intType)) {
+				error_report("Trying to assign non-int value: " + expr.getTerm().struct + "  in expr", expr);
+				expr.struct = noType;
 				return;
 			}
 		}
@@ -118,9 +202,10 @@ public class SemanticAnalyzer extends VisitorAdaptor {
 			designatorArr.obj = Tab.noObj;
 			return;
 		}
-		if(!designatorArr.getExpr().struct.equals(Tab.intType)) {
+		if(!designatorArr.getExpr().struct.equals(intType)) {
 			error_report("Indexing with non-int value", designatorArr);
 			designatorArr.obj = Tab.noObj;
+			return;
 		}
 //		In runtime for [$]
 		designatorArr.obj = new Obj(Obj.Elem, designatorArr.getDesignatorArrName().obj.getName() + "[$]", designatorArr.getDesignatorArrName().obj.getType().getElemType());
@@ -134,7 +219,7 @@ public class SemanticAnalyzer extends VisitorAdaptor {
 //		visit niz[] -> elem(strukt int)
 		Obj desObj = Tab.find(designatorArrName.getDName());
 		if(desObj == Tab.noObj) {
-			error_report("Accessing undefined var, array: " + designatorArrName.getDName(), designatorArrName);
+			error_report("Accessing undefined array: " + designatorArrName.getDName(), designatorArrName);
 			designatorArrName.obj = Tab.noObj;
 			return;
 		} 
@@ -144,20 +229,19 @@ public class SemanticAnalyzer extends VisitorAdaptor {
 			return;			
 		}
 		designatorArrName.obj = desObj;
-		
 	}
 
 //	Factor
 	
 	@Override
 	public void visit(FactorOpNew factorOpNew) {
-		if(!factorOpNew.getExpr().struct.equals(Tab.intType)) {
+		if(!factorOpNew.getExpr().struct.equals(intType)) {
 			error_report("Size of array is not of integer type", factorOpNew);
-			factorOpNew.struct = Tab.noType;
+			factorOpNew.struct = noType;
 			return;
 		}
 //		As we previously defined, type is saved to local variable when it's visited
-		factorOpNew.struct = new Struct(Struct.Array, currentType);
+		factorOpNew.struct = new Struct(Struct.Array, factorOpNew.getType().struct);
 	}
 	
 	@Override
@@ -172,8 +256,9 @@ public class SemanticAnalyzer extends VisitorAdaptor {
 
 	@Override
 	public void visit(FactorOpNumber factorNumber) {
-		factorNumber.struct = Tab.intType;
+		factorNumber.struct = intType;
 	}
+	
 	@Override
 	public void visit(FactorOpDesignator factorOpDesignator) {
 		factorOpDesignator.struct = factorOpDesignator.getDesignator().obj.getType();
@@ -181,22 +266,32 @@ public class SemanticAnalyzer extends VisitorAdaptor {
 	
 	@Override
 	public void visit(FactorOpChar factorChar) {
-		factorChar.struct = Tab.charType;
+		factorChar.struct = charType;
+	}
+	
+	@Override
+	public void visit(FactorOpRange factorOpRange) {
+		if(!factorOpRange.getExpr().struct.equals(intType)) {
+			error_report("Expression must be of type integer in function range", factorOpRange);
+			factorOpRange.struct = noType;
+			return;
+		}
+		factorOpRange.struct = new Struct(Struct.Array, intType);
 	}
 
 	
 	@Override
 	public void visit(Factor factor) {
-		if(factor.getFactorSign() instanceof FactorSignMinus) {
-			if(factor.getFactorOp().struct.equals(Tab.intType)) {
-				factor.struct = Tab.intType;
-			} else {
-				error_report("Negation of non integer value: " + factor.getFactorOp().struct.toString(), factor);
-				factor.struct = Tab.noType;
-			}
-		} else {
+		if(factor.getFactorSign() instanceof FactorSignPlus) {
 			factor.struct = factor.getFactorOp().struct;
+			return;
+		} 
+		if(factor.getFactorOp().struct.equals(intType)) {
+			factor.struct = intType;
+			return;
 		}
+		error_report("Negation of non integer value: " + factor.getFactorOp().struct.toString(), factor);
+		factor.struct = noType;
 	}
 	
 	
@@ -213,13 +308,13 @@ public class SemanticAnalyzer extends VisitorAdaptor {
 		Tab.closeScope();
 		myProg = null;
 		if(!ismain)
-			error_report("No main method", program);
+			error_report("No main method in the program", program);
 	}
 	
 	@Override
 	public void visit(ProgramName programName) {
 //		Creating program scope
-		myProg = Tab.insert(Obj.Prog, programName.getPName(), Tab.noType);
+		myProg = Tab.insert(Obj.Prog, programName.getPName(), noType);
 		Tab.openScope();
 	}
 	
@@ -228,9 +323,11 @@ public class SemanticAnalyzer extends VisitorAdaptor {
 	public void visit(VarDeclarationVar varDeclarationVar) {
 		Obj varObj = null;
 		if(currentMethod == null) {
+//			Finds in all scopes, not just open one
+//			Returns Tab.noObj;
 			varObj = Tab.find(varDeclarationVar.getVName());
 		} else {
-//			Finds in all scopes, not just open one
+//			returns null
 			varObj = Tab.currentScope().findSymbol(varDeclarationVar.getVName());
 		}
 		if(varObj != null)
@@ -239,14 +336,21 @@ public class SemanticAnalyzer extends VisitorAdaptor {
 				return;
 			} 
 		Tab.insert(Obj.Var, varDeclarationVar.getVName(), currentType);
+		if(currentMethod == null)
+			info_report("Global var declaration: " + varDeclarationVar.getVName(), varDeclarationVar);
+		else 
+			info_report("Local var declaration: " + varDeclarationVar.getVName() + " in method: " + currentMethod.getName(), varDeclarationVar);
 	}
 	
 	@Override
 	public void visit(VarDeclarationArray varDeclarationArray) {
 		Obj varObj = null;
 		if(currentMethod == null) {
+//			Finds in all scopes, not just open one
+//			Returns Tab.noObj;
 			varObj = Tab.find(varDeclarationArray.getArrName());
 		} else {
+//			returns null
 			varObj = Tab.currentScope().findSymbol(varDeclarationArray.getArrName());
 		}
 		if(varObj != null)
@@ -255,14 +359,35 @@ public class SemanticAnalyzer extends VisitorAdaptor {
 				return;
 			} 
 		Tab.insert(Obj.Var, varDeclarationArray.getArrName(), new Struct(Struct.Array, currentType));
+		if(currentMethod == null)
+			info_report("Global array declaration: " + varDeclarationArray.getArrName(), varDeclarationArray);
+		else 
+			info_report("Local array declaration: " + varDeclarationArray.getArrName() + " in method: " + currentMethod.getName(), varDeclarationArray);
 	}
 	
 //	Method declarations
 	public void visit(MethodName methodName) {
-//		For A, we don't have to check if multiple methods
-		if(methodName.getMName().equalsIgnoreCase("main")) 
-			ismain  = true;			
-		currentMethod = Tab.insert(Obj.Meth, methodName.getMName(), Tab.noType);
+		Obj metObj = null;
+		metObj = Tab.find(methodName.getMName());
+		if(metObj != Tab.noObj) {
+			if(metObj.getKind() == Obj.Meth) {
+				error_report("There is method with the name: " + methodName.getMName() + " already defined", methodName);
+				currentMethod = null;
+				return;
+			}
+			if(metObj.getKind() == Obj.Var || metObj.getKind() == Obj.Con) {
+				error_report("There is variable or const already defined, with the name: " + methodName.getMName(), methodName);
+				currentMethod = null;
+				return;
+			}
+		}
+		if(methodName.getMName().equals("main")) {
+			if(ismain) {
+				error_report("Main method declared more than once", methodName);
+			}
+			ismain = true;
+		}			
+		currentMethod = Tab.insert(Obj.Meth, methodName.getMName(), noType);
 		Tab.openScope();
 	}
 	
@@ -282,50 +407,56 @@ public class SemanticAnalyzer extends VisitorAdaptor {
 		Obj typeObj = Tab.find(type.getTName());
 		if(typeObj == Tab.noObj) {
 			error_report("Type is unknown: " + type.getTName(), type);
-			currentType = Tab.noType;
+			currentType = noType;
 			return;
 		}else if(typeObj.getKind() != Obj.Type) {
-			error_report("Unadequate type: " + type.getTName(), type);
-			currentType = Tab.noType;
+			error_report("This: "  + type.getTName() + " is not of type", type);
+			currentType = noType;
 			return;
 		}
 		currentType = typeObj.getType();
+		type.struct = typeObj.getType();
 	}
 	
 	@Override
 	public void visit(TypeConstNum typeConstNum) {
 		constant = typeConstNum.getConstVal();
-		constantType = Tab.intType;
+//		constantType = intType;
+		typeConstNum.struct = intType;
 	}
 	
 	@Override
 	public void visit(TypeConstChar typeConstChar) {
 		constant = typeConstChar.getConstVal();
-		constantType = Tab.charType;
+//		constantType = charType;
+		typeConstChar.struct = charType;
 	}
 	
 	@Override
 	public void visit(TypeConstBool typeConstBool) {
 		constant = typeConstBool.getConstVal();
 //		Tab.find(bool)
-		constantType = boolType;
+//		constantType = boolType;
+		typeConstBool.struct = boolType;
 	}
 	
 	@Override
 	public void visit(ConstDeclAssign constDeclAssign) {
-//		Is constant there
+//		Is constant already in the symbol table
 		if(Tab.find(constDeclAssign.getVName()) != Tab.noObj) {
 			error_report("Double definition of constant: " + constDeclAssign.getVName(), constDeclAssign);
 			return;
-		} else if(!constantType.assignableTo(currentType)) {
+		} else if(!constDeclAssign.getTypeConst().struct.assignableTo(currentType)) {
 			error_report("Trying to assign: " + constantType.getKind() + " to: " + currentType.getKind(), constDeclAssign);
 			return;
 		}
 //		Equals, compatible with, assignable to with struct types comparison
 //		We can go through parents, but we would need to do it in while loop, because we don't know where Type is
+//		Because we can declare only one type in one declaration[const int pi, e, g;] we can save that type and then use it here
 		Obj conObj = Tab.insert(Obj.Con, constDeclAssign.getVName(), currentType);
 //		We must write value to adr field for constants
 		conObj.setAdr(constant);
+		info_report("Constant declaration: " + constDeclAssign.getVName(), constDeclAssign);
 	}
 	
 	
